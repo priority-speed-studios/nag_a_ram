a4792cb855ba5208207317cbdf03f67b7b737a78
phoenix-zero <anubhab.mukherjee2013@gmail.com>
2019-04-18 21:25:20 +0530

    Changed directory structure Suggestion by github.com/Snowden1738 & github.com/2bit-hack

--- src/main.c
+++ src/main.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 
+#define widgetName(a) #a
 
+typedef union arko_is_star
+{
+    int seed;
+    char string[100 + sizeof (int)];
+} arko;
+
+static arko hello;
 
 static GtkBuilder* builder;
 
 void check()
 {
-    const char* inputString = gtk_entry_get_text(GTK_ENTRY(gtk_builder_get_object(builder,"inputLabel")));
-    char outputString[100] = ""; //need a way to decide the string size
+    printf("%s%d\n", hello.string + sizeof (int), hello.seed);
+    const char* inputString = gtk_entry_get_text(
+                GTK_ENTRY(gtk_builder_get_object(builder, widgetName(inputLabel))));
+    char outputString[100] = "", newString[400]= ""; //need a way to decide the string size
     //breakpoint
-    strcpy(outputString,inputString); //replace strcpy with arko's stuff
+    strcpy(outputString, inputString); //replace strcpy with arko's stuff
     //breakpoint
-    GtkTextBuffer* buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(gtk_builder_get_object(builder,"outputLabel")));
-    GtkTextIter end,start;
-    gtk_text_buffer_get_end_iter(buffer,&end);
-    gtk_text_buffer_get_start_iter(buffer,&start);
-    gtk_text_buffer_delete(buffer,&start,&end);;
-    gtk_text_buffer_insert(buffer,&end,outputString,-1);
+    GtkTextBuffer* buffer = gtk_text_view_get_buffer(
+                GTK_TEXT_VIEW(gtk_builder_get_object(builder, widgetName(outputLabel))));
+    sprintf(newString, "%s\n%s\n%s", outputString, outputString, outputString);
+    GtkTextIter end, start;
+    gtk_text_buffer_get_end_iter(buffer, &end);
+    gtk_text_buffer_get_start_iter(buffer, &start);
+    gtk_text_buffer_delete(buffer, &start, &end);
+    gtk_text_buffer_insert(buffer, &end, newString, -1);
 }
 
-int main(int argc,char** argv)
+int main(int argc, char** argv)
 {
-    gtk_init(&argc,&argv);
-    builder = gtk_builder_new_from_file("./mainWindow.glade");
-    GtkWindow* window= GTK_WINDOW(gtk_builder_get_object(builder,"window_main"));
+    gtk_init(&argc, &argv);
+    hello.seed = 4;
+    strcpy(hello.string + sizeof (int), "Yay! It works.\n");
+    builder = gtk_builder_new_from_file(widgetName(./mainWindow.glade));
+    GtkWindow* window= GTK_WINDOW(gtk_builder_get_object(builder, widgetName(window_main)));
     gtk_widget_show(GTK_WIDGET(window));
-    gtk_builder_connect_signals(builder,NULL);
+    gtk_builder_connect_signals(builder, NULL);
     gtk_main();
+    g_object_unref(builder);
     return 0;
 }
