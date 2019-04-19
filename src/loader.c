ff3ab7d6bab1ea737af36702d4afd8d402b16de9
phoenix-zero <anubhab.mukherjee2013@gmail.com>
2019-04-19 19:00:14 +0530

    Added testFiles Removed bug

--- src/loader.c
+++ src/loader.c
@@ -40,20 +40,17 @@ void plugin_set(GtkFileChooserButton *button)
         gtk_widget_destroy (dialog);
         return;
     }
-    if(!strcmp(wordFile,""))
+    int code = setupf(wordFile);
+    if(code)
     {
-        int code = setupf(wordFile);
-        if(code)
-        {
-            GtkWidget* dialog = gtk_message_dialog_new (NULL,
-                                                        GTK_DIALOG_DESTROY_WITH_PARENT,
-                                                        GTK_MESSAGE_ERROR,
-                                                        GTK_BUTTONS_CLOSE,
-                                                        "Error: %d",
-                                                        code);
-            gtk_dialog_run (GTK_DIALOG (dialog));
-            gtk_widget_destroy (dialog);
-        }
+        GtkWidget* dialog = gtk_message_dialog_new (NULL,
+                                                    GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                    GTK_MESSAGE_ERROR,
+                                                    GTK_BUTTONS_CLOSE,
+                                                    "Error: %d",
+                                                    code);
+        gtk_dialog_run (GTK_DIALOG (dialog));
+        gtk_widget_destroy (dialog);
     }
     algorithm = task;
     setup = setupf;
