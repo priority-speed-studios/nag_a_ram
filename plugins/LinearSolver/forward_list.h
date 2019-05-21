4adc4fbc902e8fad2c9b6570780a851107856929
Arkajyoti Banerjee <34052538+YesPreserveRoot@users.noreply.github.com>
2019-05-21 13:26:10 +0530

    Added ctor-like functions and function prototypes
    Added ctor-like functions and re-formatted with function prototypes

--- plugins/LinearSolver/forward_list.h
+++ plugins/LinearSolver/forward_list.h
@@ -7,18 +7,39 @@ typedef struct _node
 	struct _node* next;
 } node;
 
+node* node_new(char*);
+
 typedef struct _forward_list
 {
 	node *begin, *end;
 } forward_list;
 
-void insert(forward_list* var, char* s) // const char* works as well
+forward_list* forward_list_new();
+void insert(forward_list*, char*);
+
+node* node_new(char* s)
 {
 	node* temp = (node*) malloc(sizeof(node));
-	temp -> s = (char*) malloc(sizeof(char) * (strlen(s) + 1));
 	
+	temp -> s = (char*) malloc(sizeof(char) * (strlen(s) + 1));
 	strcpy(temp -> s, s);
+	
 	temp -> next = NULL;
+	
+	return temp;
+}
+
+forward_list* forward_list_new()
+{
+	forward_list* temp = (forward_list*) malloc(sizeof(forward_list));
+	temp -> begin = temp -> end = NULL;
+	
+	return temp;
+}
+
+void insert(forward_list* var, char* s) // const char* works as well
+{
+	node* temp = node_new(s);
 
 	if(var -> begin)
 		var -> end = var -> end -> next = temp;
