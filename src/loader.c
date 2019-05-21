6a2d6d49336ab4322f96f9c341ac825ef4161680
Anubhab Mukherjee <anubhab.mukherjee2013@gmail.com>
2019-05-21 14:02:29 +0530

    Fixed dangling symlinks

--- src/loader.c
+++ src/loader.c
@@ -17,8 +17,8 @@ void plugin_set()
     if(pluginsState)
         return;
     char fileName[50] = "./plugs/";
-    strcat(fileName,gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(
-                                           gtk_builder_get_object(builder,widgetName(algosbox)))));
+    strcat(fileName,gtk_combo_box_text_get_active_text(
+               GTK_COMBO_BOX_TEXT(gtk_builder_get_object(builder,widgetName(algosbox)))));
     void * dlh = dlopen(fileName, RTLD_NOW);
     if (!dlh)
     {
